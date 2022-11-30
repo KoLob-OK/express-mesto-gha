888a7419d8fb222fb4e13a215bc2229754838556
KoLobOK <webkolobok@mail.ru>
2022-11-30 11:16:46 +0300

    feat: create schema and model for user and card

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
-/* const mongoose = require('mongoose');
-const path = require('path'); */
+const mongoose = require('mongoose');
+const path = require('path');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 app.listen(PORT, () => {
   console.log(`App  listening on port ${PORT}`);

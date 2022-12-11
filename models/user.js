8eba0036fb7931018bb7d213616ec1451bfb637d
KoLobOK <webkolobok@mail.ru>
2022-12-11 12:02:47 +0300

    feat: add email and password to user schema

--- models/user.js
+++ models/user.js
@@ -1,6 +1,21 @@
 const mongoose = require('mongoose');
+const isEmail = require('validator/lib/isEmail');
 
 const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: isEmail,
+      message: 'Неверный формат данных',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
   name: {
     type: String,
     required: true,
